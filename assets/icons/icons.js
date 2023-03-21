67a9d3ee95d3fb6daf9663e0ee35c48072465ccc
Соловьева Анастасия <soloveva_a_qschool@qsoft.ru>
2023-03-21 15:55:59 +0000

    247750/добавила Scss, переменные, шрифты

--- assets/icons/icons.js
+++ assets/icons/icons.js
@@ -1,2 +1,15 @@
 // Пример подключения иконки
 // import './svg/my-icon.svg';
+
+import './svg/account.svg';
+import './svg/arrow-right.svg';
+import './svg/comments.svg';
+import './svg/info.svg';
+import './svg/instagram.svg';
+import './svg/like.svg';
+import './svg/person.svg';
+import './svg/question.svg';
+import './svg/search.svg';
+import './svg/star-empty.svg';
+import './svg/star-fill.svg';
+import './svg/youtube.svg';
